f3bcb715d18bfd03cb8ef0d98265230cee039ee7
Navid M <navidmomtahen@gmail.com>
2025-03-30 18:09:25 +0100

    docs

--- src/shark/processing.nim
+++ src/shark/processing.nim
@@ -2,6 +2,7 @@ import std/os
 import text
 
 proc process_file*(filename: string, to_camel: bool) =
+    ## Convert some text-based file to camel or snake case.
     if not file_exists(filename):
         echo "File not found: ", filename
         return
